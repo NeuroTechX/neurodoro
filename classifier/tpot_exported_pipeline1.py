4487cb27078fc02ed5fcaf3b5c3a3a2b3291b01a
jdpigeon <morrisondano@gmail.com>
2017-07-17 21:17:06 -0400

    First TPOT Regressor run

---
+++ classifier/tpot_exported_pipeline1.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from sklearn.linear_model import LassoLarsCV
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
+
+# NOTE: Make sure that the class is labeled 'class' in the data file
+tpot_data = np.recfromcsv('PATH/TO/DATA/FILE', delimiter='COLUMN_SEPARATOR', dtype=np.float64)
+features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpot_data.dtype.names.index('class'), axis=1)
+training_features, testing_features, training_target, testing_target = \
+    train_test_split(features, tpot_data['class'], random_state=42)
+
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=LassoLarsCV(normalize=True)),
+    KNeighborsRegressor(n_neighbors=61, weights="uniform")
+)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
