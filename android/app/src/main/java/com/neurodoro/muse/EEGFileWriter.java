376a68fe3feed6dfca724d5eda270676f78c3cf6
jdpigeon <morrisondano@gmail.com>
2017-11-16 20:45:00 -0500

    Sending CORVOSession to React as JSON

--- android/app/src/main/java/com/neurodoro/muse/EEGFileWriter.java
+++ android/app/src/main/java/com/neurodoro/muse/EEGFileWriter.java
@@ -8,14 +8,19 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.iid.InstanceID;
+import com.google.gson.Gson;
+import com.neurodoro.cloud.CORVOSession;
 import com.neurodoro.cloud.CloudStorageHelper;
 // import com.neurodoro.cloud.CloudStorageHelper;
+
+import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.security.GeneralSecurityException;
 
 /**
@@ -23,171 +28,51 @@ import java.security.GeneralSecurityException;
  * recording is started and sends data to Neurodoro data base when recording is completed
  */
 public class EEGFileWriter {
-
-  // ---------------------------------------------------------------------------
-  // Variables
-
   private Context context;
-  private String title;
-  StringBuilder builder;
-  int fileNum = 1;
-  private int nbFreqBins = 0;
-  public FileWriter fileWriter;
+  private String dataType;
   private static boolean isRecording;
-  private int difficulty = 0;
-  private int performance = 0;
-  private String userName = "";
-  private boolean firstLine;
-  private String instanceID;
-  private String sessionId;
+  private CORVOSession session;
+  private String testType;
 
-  // ---------------------------------------------------------------------------
-  // Constructor
+  private int[] scores;
 
-  public EEGFileWriter(Context context, String title, int fileNum) {
+  public EEGFileWriter(Context context, String dataType, String testType) {
     Log.w("MuseDataSource", "Abstract constructor called");
-    this.fileNum = fileNum;
     this.context = context;
     isRecording = false;
-    this.title = getTitleFromDataType(title);
-    instanceID = InstanceID.getInstance(context).getId();
-    sessionId = instanceID.substring(7) + Long.toString(System.currentTimeMillis()).substring(9);
-  }
+    this.dataType = getDataType(dataType);
+    this.testType = testType;
+    this.scores = new int[2];
 
-  public EEGFileWriter(Context context, String title, int fileNum, int nbFreqBins) {
-    Log.w("MuseDataSource", "Abstract constructor called");
-    this.fileNum = fileNum;
-    this.context = context;
-    this.nbFreqBins = nbFreqBins;
-    isRecording = false;
-    this.title = getTitleFromDataType(title);
-    instanceID = InstanceID.getInstance(context).getId();
-    sessionId = instanceID.substring(7) + Long.toString(System.currentTimeMillis()).substring(9);
+    session = new CORVOSession(testType, dataType);
   }
 
   // ---------------------------------------------------------------------------
   // Internal methods
 
   public void initFile() {
-    firstLine = true;
     Log.w("FileWriter", "init file called");
-    builder = new StringBuilder();
-    builder.append("Timestamp (ms),");
-    builder.append("Difficulty,");
-    builder.append("Performance,");
-    if (title.contains("PSD")) {
-      builder.append("Channel,");
-      for (int i = 1; i <= nbFreqBins; i++) {
-        builder.append(i + " hz,");
-      }
-    } else {
-      for (int i = 1; i <= 4; i++) {
-        builder.append("Channel " + i + ",");
-      }
-    }
-
-    // Create unique Instance ID column
-    builder.append("Instance ID,");
-
-    // Create unique Session ID column;
-    builder.append("Session ID,");
-
-    // Create dataType column
-    builder.append("Data Type,");
-
-    builder.append("\n");
-    makeToast("Started recording");
-
+    session = new CORVOSession(testType, dataType);
     isRecording = true;
   }
 
-  public void addEEGDataToFile(double[] data) {
-    if (firstLine == false) {
-      return;
-    }
-
-    // Append timestamp
-    Long tsLong = System.currentTimeMillis();
-    builder.append(tsLong.toString() + ",");
-
-    // Append task info
-    builder.append(this.difficulty + ",");
-    builder.append(this.performance + ",");
-
-    // Loop through all 4 channels and write data
-
-    for (int j = 0; j < data.length; j++) {
-      builder.append(Double.toString(data[j]));
-      if (j < data.length - 1) {
-        builder.append(",");
-      }
-    }
-    if (firstLine == true) {
-      builder.append("," + instanceID + "," + sessionId + "," + title);
-      firstLine = false;
-    }
-    builder.append("\n");
+  public void addSample(double[] data) {
+    // Get relative timestamp
+    session.addSample(data, scores, (int)System.currentTimeMillis()); // Topic of bikeshedding discussion
   }
 
   public void addPSDToFile(double[][] data) {
-    // Loop through all 4 channels
-    for (int j = 0; j < data.length; j++) {
-
-      // Append timestamp
-      Long tsLong = System.currentTimeMillis();
-      builder.append(tsLong.toString() + ",");
-
-      // Append task info
-      builder.append(Integer.toString(this.difficulty) + ",");
-      builder.append(Integer.toString(this.performance) + ",");
-
-      // Append channel number
-      builder.append(j + 1 + ",");
-
-      // Loop through all freqbins
-      for (int i = 0; i < data[j].length; i++) {
-        builder.append(Double.toString(data[j][i]));
-        if (i < data[j].length) {
-          builder.append(",");
-        }
-      }
-
-      if (firstLine == true) {
-        builder.append("," + instanceID + "," + sessionId + "," + title);
-        firstLine = false;
-      }
-      builder.append("\n");
-    }
+    // How about let's not right now
+    return;
   }
 
-  public void writeFile() {
-    try {
-      final File dir = context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
-      if (!dir.exists()) {
-        dir.mkdir();
-      }
-      final File file = new File(dir,title+fileNum+
-                      ".csv");
-      fileWriter = new java.io.FileWriter(file);
-
-      BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-      bufferedWriter.write(builder.toString());
-      bufferedWriter.close();
-
-      sendData(file);
-      isRecording = false;
-      makeToast("Data written to " + sessionId + ".csv");
-    } catch (IOException e) {
-    }
+  public void stopRecording() {
+    isRecording = false;
   }
 
-  public void updateTaskInfo(int difficulty, int performance) {
-    this.difficulty = difficulty;
-    this.performance = performance;
-  }
-
-  public void updateUserName(String name) {
-    this.userName = name;
+  public void updateScore(int difficulty, int performance) {
+    scores[0] = difficulty;
+    scores[1] = performance;
   }
 
   public void makeToast(String text) {
@@ -195,7 +80,7 @@ public class EEGFileWriter {
     toast.show();
   }
 
-  private String getTitleFromDataType(String dataType) {
+  private String getDataType(String dataType) {
 
     switch (dataType) {
       case "DENOISED_PSD":
@@ -205,26 +90,13 @@ public class EEGFileWriter {
       case "RAW_EEG":
         return "RawEEG";
       default:
-        return "DenoisedPSD";
+        return "RawEEG";
     }
   }
 
-  public void sendData(File dataCSV) {
-    /*
-    Intent sendIntent = new Intent();
-    sendIntent.setAction(Intent.ACTION_SEND);
-    sendIntent.setType("application/csv");
-    sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(dataCSV));
-    sendIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    context.startActivity(Intent.createChooser(sendIntent, "Export " +
-            "data to..."));
-    */
-    CloudStorageHelper cloudHelper = new CloudStorageHelper();
-    try {
-      cloudHelper.uploadData(builder.toString());
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+  public String getCORVOSession() {
+    Gson gson = new Gson();
+    return gson.toJson(session);
   }
 
   public boolean isRecording() {
--- android/app/src/main/java/com/neurodoro/muse/MuseRecorder.java
+++ android/app/src/main/java/com/neurodoro/muse/MuseRecorder.java
@@ -9,6 +9,7 @@ import com.choosemuse.libmuse.MuseArtifactPacket;
 import com.choosemuse.libmuse.MuseDataListener;
 import com.choosemuse.libmuse.MuseDataPacket;
 import com.choosemuse.libmuse.MuseDataPacketType;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -22,265 +23,174 @@ import com.neurodoro.signal.Filter;
 import com.neurodoro.signal.NoiseDetector;
 import com.neurodoro.signal.PSDBuffer2D;
 
-/**
- * Streams data from Muses and performs preprocessing functions
- */
-
+/** Streams data from Muses and performs preprocessing functions */
 public class MuseRecorder extends ReactContextBaseJavaModule {
 
-    // ----------------------------------------------------------
-    // Variables
+  // ----------------------------------------------------------
+  // Variables
 
-    Thread dataThread;
-    public int samplingFreq;
-    public int BUFFER_LENGTH;
-    public PSDDataSource data;
-    public DataListener dataListener;
-    public CircularBuffer eegBuffer;
-    public String userName = "";
-    public int fileNum = 0;
-    public EEGFileWriter fileWriter;
+  Thread dataThread;
+  public int samplingFreq;
+  public int BUFFER_LENGTH;
+  public DataListener dataListener;
+  public CircularBuffer eegBuffer;
+  public String userName = "";
+  public int fileNum = 0;
+  public EEGFileWriter fileWriter;
+  private String testType;
+
+  // Filter variables
+  public Filter bandPassFilter;
+  public double[][] bandPassFiltState;
+
+  // Bridged props
+  public String dataType;
+
+  // Reference to global Muse
+  MainApplication appState;
+
+  // ---------------------------------------------------------
+  // Constructor
+  public MuseRecorder(ReactApplicationContext reactContext) {
+    super(reactContext);
+    appState = ((MainApplication) reactContext.getApplicationContext());
+  }
+
+  // ---------------------------------------------------------
+  // React Native Module methods
+  // Required by ReactContextBaseJavaModule
+
+  @Override
+  public String getName() {
+    return "MuseRecorder";
+  }
+
+  // Called to emit events to event listeners in JS
+  private void sendEvent(
+      ReactContext reactContext, String eventName, @Nullable WritableMap params) {
+    reactContext
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit(eventName, params);
+  }
+
+  // ---------------------------------------------------------
+  // Bridged methods
+
+  @ReactMethod
+  public void startRecording(String dataType, String testType) {
+    Log.w("Listener", "Start Listening Called");
+    this.dataType = dataType;
+    this.testType = testType;
+    initListener();
+    fileWriter.initFile();
+  }
+
+  @ReactMethod
+  public void stopRecording() {
+    Log.w("Listener", "Stop Listening Called");
+    if (dataListener != null) {
+      appState.connectedMuse.unregisterDataListener(dataListener, MuseDataPacketType.EEG);
+    }
+    fileWriter.stopRecording();
+    fileNum++;
+  }
+
+  @ReactMethod
+  public void updateScore(int difficulty, int performance) {
+    if (fileWriter != null) {
+      fileWriter.updateScore(difficulty, performance);
+    }
+  }
+
+  @ReactMethod
+  public void getCORVOSession(Callback errorCallback, Callback successCallback) {
+    try {
+      successCallback.invoke(fileWriter.getCORVOSession());
+
+    } catch (Exception e) {
+      errorCallback.invoke(e.getMessage());
+    }
+  }
+
+  // ---------------------------------------------------------
+  // Internal Methods
+
+  public void initListener() {
+    Log.w("Listener", "Init Listener Called");
+
+    // Set sampling frequency based on type of Muse
+    if (appState.connectedMuse.isLowEnergy()) {
+      samplingFreq = 256;
+    } else {
+      samplingFreq = 220;
+    }
+
+    Log.w("Listener", "EEG datatype detected");
+    fileWriter = new EEGFileWriter(getCurrentActivity(), dataType, testType);
+
+    // If data will be filtered, create filters
+    if (dataType.contains("FILTERED")) {
+      Log.w("Listener", "Filter detected");
+      bandPassFilter = new Filter(samplingFreq, "bandpass", 4, 1, 40);
+      bandPassFiltState = new double[4][bandPassFilter.getNB()];
+    }
+
+    // Register a listener to receive data.
+    dataListener = new DataListener();
+    MainApplication.connectedMuse.registerDataListener(dataListener, MuseDataPacketType.EEG);
+  }
+
+  // --------------------------------------------------------------
+  // Listeners
+
+  // Listener that receives incoming data from the Muse. Will run receiveMuseDataPacket
+  // Will call receiveMuseDataPacket as data comes in around 220hz (250hz for Muse 2016)
+  private final class DataListener extends MuseDataListener {
+    private double[] newData;
 
     // Filter variables
-    public Filter bandPassFilter;
-    public double[][] bandPassFiltState;
+    public boolean filterOn = false;
+    public Filter bandstopFilter;
+    public double[][] bandstopFiltState;
 
-    // Bridged props
-    public String recorderDataType;
-
-    // Reference to global Muse
-    MainApplication appState;
-
-    // ---------------------------------------------------------
-    // Constructor
-    public MuseRecorder(ReactApplicationContext reactContext) {
-        super(reactContext);
-        appState = ((MainApplication)reactContext.getApplicationContext());
+    DataListener() {
+      if (appState.connectedMuse.isLowEnergy()) {
+        filterOn = true;
+        bandstopFilter = new Filter(256, "bandstop", 5, 45, 55);
+        bandstopFiltState = new double[4][bandstopFilter.getNB()];
+      }
+      newData = new double[4];
     }
-
-    // ---------------------------------------------------------
-    // React Native Module methods
-    // Required by ReactContextBaseJavaModule
 
     @Override
-    public String getName() {
-        return "MuseRecorder";
+    public void receiveMuseDataPacket(final MuseDataPacket p, final Muse muse) {
+      getEegChannelValues(newData, p);
+
+      // BandStop filter for 2016 Muses
+      if (filterOn) {
+        bandstopFiltState = bandstopFilter.transform(newData, bandstopFiltState);
+        newData = bandstopFilter.extractFilteredSamples(bandstopFiltState);
+      }
+
+      // Optional filter for filtered data
+      if (dataType.contains("FILTERED")) {
+        bandPassFiltState = bandPassFilter.transform(newData, bandPassFiltState);
+        newData = bandPassFilter.extractFilteredSamples(bandPassFiltState);
+      }
+
+      fileWriter.addSample(newData);
     }
 
-    // Called to emit events to event listeners in JS
-    private void sendEvent(ReactContext reactContext, String eventName, @Nullable WritableMap params) {
-        reactContext
-                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit(eventName, params);
+    private void getEegChannelValues(double[] buffer, MuseDataPacket p) {
+      buffer[0] = p.getEegChannelValue(Eeg.EEG1);
+      buffer[1] = p.getEegChannelValue(Eeg.EEG2);
+      buffer[2] = p.getEegChannelValue(Eeg.EEG3);
+      buffer[3] = p.getEegChannelValue(Eeg.EEG4);
     }
 
-    // ---------------------------------------------------------
-    // Bridged methods
-
-    @ReactMethod
-    public void startRecording(String dataType) {
-        Log.w("Listener", "Start Listening Called");
-        this.recorderDataType = dataType;
-        initListener();
-        fileWriter.initFile();
-        if(recorderDataType.contains("DENOISED_PSD")) {
-            startDataThread();
-        }
+    @Override
+    public void receiveMuseArtifactPacket(final MuseArtifactPacket p, final Muse muse) {
+      // Does nothing for now
     }
-
-    @ReactMethod
-    public void stopRecording() {
-        Log.w("Listener", "Stop Listening Called");
-        if (dataListener != null) {
-            appState.connectedMuse.unregisterDataListener(dataListener, MuseDataPacketType.EEG);
-        }
-        if(recorderDataType.contains("DENOISED_PSD")) {
-            stopThread();
-        }
-        fileWriter.writeFile();
-        fileNum++;
-    }
-
-    @ReactMethod
-    public void sendTaskInfo(int difficulty, int performance) {
-        if (fileWriter != null) {
-            fileWriter.updateTaskInfo(difficulty, performance);
-        }
-    }
-
-    @ReactMethod
-    public void setUserName(String name) {
-        this.userName = name;
-    }
-
-    // ---------------------------------------------------------
-    // Internal Methods
-
-    public void initListener() {
-        Log.w("Listener", "Init Listener Called");
-
-        // Set sampling frequency based on type of Muse
-        if (appState.connectedMuse.isLowEnergy()) {
-            samplingFreq = 256;
-        } else { samplingFreq = 220; }
-
-        // Create PSDDataSource if recorder is set to record PSD data.
-        // This will create a different type of PSDDataSource
-        if (recorderDataType.contains("PSD")) {
-            Log.w("Listener", "PSD datatype detected");
-            eegBuffer = new CircularBuffer(samplingFreq, 4);
-            data = new PSDDataSource();
-        } else {
-            Log.w("Listener", "EEG datatype detected");
-            fileWriter = new EEGFileWriter(getCurrentActivity(), recorderDataType, fileNum);
-
-            // If data will be filtered, create filters
-            if(recorderDataType.contains("FILTERED")) {
-                Log.w("Listener", "Filter detected");
-                bandPassFilter = new Filter(samplingFreq, "bandpass", 4, 1, 40);
-                bandPassFiltState = new double[4][bandPassFilter.getNB()];
-            }
-        }
-
-        // Register a listener to receive connection state changes.
-        dataListener = new DataListener();
-        appState.connectedMuse.registerDataListener(dataListener, MuseDataPacketType.EEG);
-    }
-
-    // ---------------------------------------------------------
-    // Thread management functions
-
-    public void startDataThread() {
-        dataThread = new Thread(data);
-        dataThread.start();
-    }
-
-    public void stopThread(){
-        if(data != null) {
-            data.stopThread();
-        }
-    }
-
-    // --------------------------------------------------------------
-    // Listeners
-
-    // Listener that receives incoming data from the Muse. Will run receiveMuseDataPacket
-    // Will call receiveMuseDataPacket as data comes in around 220hz (250hz for Muse 2016)
-    private final class DataListener extends MuseDataListener {
-        private double[] newData;
-
-        // Filter variables
-        public boolean filterOn = false;
-        public Filter bandstopFilter;
-        public double[][] bandstopFiltState;
-
-        DataListener() {
-            if (appState.connectedMuse.isLowEnergy()) {
-                filterOn = true;
-                bandstopFilter = new Filter(256, "bandstop", 5, 45, 55);
-                bandstopFiltState = new double[4][bandstopFilter.getNB()];
-            }
-            newData = new double[4];
-        }
-
-        @Override
-        public void receiveMuseDataPacket(final MuseDataPacket p, final Muse muse) {
-            getEegChannelValues(newData, p);
-
-            // BandStop filter for 2016 Muses
-            if(filterOn) {
-                bandstopFiltState = bandstopFilter.transform(newData, bandstopFiltState);
-                newData = bandstopFilter.extractFilteredSamples(bandstopFiltState);
-            }
-
-            // Optional filter for filtered data
-            if(recorderDataType.contains("FILTERED")) {
-                bandPassFiltState = bandPassFilter.transform(newData, bandPassFiltState);
-                newData = bandPassFilter.extractFilteredSamples(bandPassFiltState);
-            }
-
-            if(recorderDataType.contains("DENOISED_PSD")) {
-                eegBuffer.update(newData);
-            } else {
-                fileWriter.addEEGDataToFile(newData);
-            }
-        }
-
-        private void getEegChannelValues(double[] buffer, MuseDataPacket p) {
-            buffer[0] = p.getEegChannelValue(Eeg.EEG1);
-            buffer[1] = p.getEegChannelValue(Eeg.EEG2);
-            buffer[2] = p.getEegChannelValue(Eeg.EEG3);
-            buffer[3] = p.getEegChannelValue(Eeg.EEG4);
-        }
-
-        @Override
-        public void receiveMuseArtifactPacket(final MuseArtifactPacket p, final Muse muse) {
-            // Does nothing for now
-        }
-    }
-
-    // --------------------------------------------------------------
-    // Runnables
-
-    // Runs an FFT on data in eegBuffer when it fills up, checks for noise, and writes data to
-    // csv file if it is noise-free
-    public final class PSDDataSource implements Runnable  {
-        private int nbChannels = 4;
-        private boolean keepRunning;
-        private NoiseDetector noiseDetector;
-        private boolean[] noiseArray;
-        int inputLength = samplingFreq * 1; // 1s second samples
-        int fftLength = 256;
-        private FFT fft = new FFT(inputLength, fftLength, samplingFreq);
-        int fftBufferLength = 20;
-        int nbFreqBins = fft.getFreqBins().length;
-        PSDBuffer2D psdBuffer = new PSDBuffer2D(fftBufferLength, nbChannels, nbFreqBins);
-        public double[][] latestSamples = new double[nbChannels][inputLength];
-        public double[][] smoothLogPower = new double[nbChannels][nbFreqBins];
-
-        public PSDDataSource() {
-            fileWriter = new EEGFileWriter(getCurrentActivity(), recorderDataType, fileNum, nbFreqBins);
-            fileWriter.updateUserName(userName);
-            noiseDetector = new NoiseDetector(600.0);
-        }
-
-        @Override
-        public void run() {
-            try {
-                keepRunning = true;
-                while (keepRunning) {
-                    if (eegBuffer.getPts() >= inputLength) {
-                        latestSamples = eegBuffer.extractTransposed(inputLength);
-                        noiseArray = noiseDetector.detectArtefact(latestSamples);
-
-                        if (isNoiseFree(noiseArray)) {
-                            psdBuffer.fftAndUpdate(latestSamples, fft);
-                            smoothLogPower = psdBuffer.mean();
-                            fileWriter.addPSDToFile(smoothLogPower);
-                        }
-
-                        eegBuffer.resetPts();
-                    }
-                }
-            } catch (Exception e) {Log.e("MuseRecorder", "Error in runnable " + e);}
-        }
-
-        public boolean isNoiseFree(boolean[] array)
-        {
-            for(int i = 0; i < array.length; i++) if(array[i]) {
-                Log.w("Listener", "Noise detected in electrode " + (i + 1) );
-                return false;
-            }
-            return true;
-        }
-
-        public void stopThread() {
-            keepRunning = false;
-        }
-
-        public void clearDataBuffer() {
-            psdBuffer.clear();
-            eegBuffer.clear();
-        }
-    }
+  }
 }
