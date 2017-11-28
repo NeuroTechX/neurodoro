942c333e0ff537af589cdf9fd1bfbcf6d24f14b7
jdpigeon <morrisondano@gmail.com>
2017-11-27 19:31:11 -0500

    Added release signing

--- android/app/src/main/java/com/neurodoro/MainApplication.java
+++ android/app/src/main/java/com/neurodoro/MainApplication.java
@@ -25,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+      return com.neurodoro.BuildConfig.DEBUG;
     }
 
     @Override
